801018487b61cde85fe93a6cc52d279805a2248c
Armando Rios <armandorios.dev@gmail.com>
2024-12-16 21:22:31 -0800

    feat: add id on book constructor

--- src/createBook.js
+++ src/createBook.js
@@ -1,4 +1,5 @@
-function Book(title, author, pages) {
+function Book(id, title, author, pages) {
+  this.id = id
   this.title = title
   this.author = author
   this.pages = pages
@@ -7,7 +8,14 @@ function Book(title, author, pages) {
 const books = JSON.parse(localStorage.getItem("books"))
 
 export function createBook(title, author, pages) {
-  const book = new Book(title, author, pages)
+  const generateId = () => {
+    const timestamp = ((new Date().getTime() / 1000) | 0).toString(16);
+    const randomness = Math.random().toString(16).substring(2, 8);
+    return timestamp + randomness;
+  };
+
+  const id = generateId(); // Combina timestamp + números aleatorios
+  const book = new Book(id, title, author, pages)
 
   books.push(book);
 
