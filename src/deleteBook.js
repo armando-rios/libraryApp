0be0207988c9782cbf70d4ff749b50b6e795e177
Armando Rios <armandorios.dev@gmail.com>
2024-12-17 12:21:04 -0800

    fix: delete unnecessary console.log()

--- src/deleteBook.js
+++ src/deleteBook.js
@@ -1,6 +1,5 @@
 export const deleteBook = (id) => {
   const books = JSON.parse(localStorage.getItem("books"))
-  console.log(id)
   const newBook = books.filter((book) => book.id != id)
   localStorage.setItem("books", JSON.stringify(newBook))
 }
