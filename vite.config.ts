1a8b8dc61222a7812d770fa13bcea93672cad401
A R <armandorios.dev@gmail.com>
2025-01-25 20:09:08 -0800

    chore: update tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import tailwindcss from 'tailwindcss/vite'
 export default defineConfig({
   base: '/libraryApp/',
   build: {
     outDir: 'dist',
   },
+  plugins: [
+    tailwindcss()
+  ],
 })
